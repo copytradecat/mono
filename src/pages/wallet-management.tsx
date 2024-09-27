86abe479e87fed65805dee9b4caae46d024fd3fb
friend <friend@copytradecat.com>
2024-09-26 18:12:22 -0700

    fix: wallethandler error

--- src/pages/wallet-management.tsx
+++ src/pages/wallet-management.tsx
@@ -1,6 +1,5 @@
 import { useSession } from 'next-auth/react';
 import WalletManagement from '../components/WalletManagement';
-import WalletHeader from '../components/WalletHeader';
 import { useRouter } from 'next/router';
 
 export default function WalletManagementPage() {
@@ -18,7 +17,6 @@ export default function WalletManagementPage() {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <WalletHeader />
       <h1 className="text-3xl font-bold mb-8">Wallet Management</h1>
       <WalletManagement />
     </div>
